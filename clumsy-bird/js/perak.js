6c7c5564952bfc401299cef560b006ddc408f326
Rendy Arya Kemal <renrror@gmail.com>
2022-01-24 14:22:48 +0700

    Add IPC, (d)encryption

--- clumsy-bird/js/perak.js
+++ clumsy-bird/js/perak.js
@@ -3,9 +3,22 @@ var gameData = {
     points: [],
     keystrokes: [],
 };
+var encKey = "default-key"
 
 function uploadGameData() {
-    console.log(gameData);
+    console.log(encKey)
+    var ciphertext = CryptoJS.AES.encrypt(JSON.stringify(gameData), encKey).toString();
+    var result = pako.gzip(ciphertext)
+    window.top.postMessage(result, '*')
+    console.log(result);
+    console.log(decrypt(result, encKey))
+}
+
+function decrypt(compressedResult, encKey) {
+    var decoder = new TextDecoder();
+    const decompressed = pako.inflate(compressedResult)
+    const decryptedBytes = CryptoJS.AES.decrypt(decoder.decode(decompressed), encKey)
+    return JSON.parse(decryptedBytes.toString(CryptoJS.enc.Utf8))
 }
 
 function reset() {
@@ -15,3 +28,10 @@ function reset() {
         keystrokes: [],
     };
 }
+
+window.onmessage = function (e) {
+    if (e.data.startsWith('k')) {
+        encKey = e.data.split("k")[1]
+    }
+};
+
