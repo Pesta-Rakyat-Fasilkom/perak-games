19cf79926fda8e9e13465f5370314f24976e4db2
Rendy Arya Kemal <renrror@gmail.com>
2022-02-11 18:39:11 +0700

    Send onReady, better parsing and data send

--- clumsy-bird/js/perak.js
+++ clumsy-bird/js/perak.js
@@ -9,7 +9,7 @@ function uploadGameData() {
     console.log(encKey)
     var ciphertext = CryptoJS.AES.encrypt(JSON.stringify(gameData), encKey).toString();
     var result = pako.gzip(ciphertext)
-    window.top.postMessage(result, '*')
+    window.top.postMessage({score: gameData.score, d: result}, '*')
     console.log(result);
     console.log(decrypt(result, encKey))
 }
@@ -30,8 +30,15 @@ function reset() {
 }
 
 window.onmessage = function (e) {
-    if (e.data.startsWith('k')) {
-        encKey = e.data.split("k")[1]
+    console.log("New message: " + e.data)
+    if (e.data.startsWith('k:')) {
+        encKey = e.data.split("k:")[1]
+        console.log("Received key: " + encKey)
     }
+    if (e.data == "reload")
+        window.location.reload()
 };
 
+(function () {
+    window.top.postMessage("ready", '*')
+})()
