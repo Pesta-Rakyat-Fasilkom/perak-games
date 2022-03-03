50bfec7ea4dbaf9c1864756d44e3638071963fa2
Rendy Arya Kemal <renrror@gmail.com>
2022-03-03 14:40:18 +0700

    Stop game on countdown finish

--- typeracer/src/components/Timer.tsx
+++ typeracer/src/components/Timer.tsx
@@ -4,22 +4,22 @@ import { useGameContext } from '../context/GameContext'
 
 const Timer: FunctionalComponent = () => {
     const [countdown, setCountdown] = useState(30)
+    const [intervalId, setIntervalId] = useState(0)
     const { startTime, stopGame } = useGameContext()
 
-    let interval: number
     useEffect(() => {
         if (startTime == 0) return
-        interval = setInterval(() => {
-            setCountdown((current) => current - 1)
-        }, 1000)
-
-        return () => clearInterval(interval)
+        setIntervalId(
+            setInterval(() => {
+                setCountdown((current) => current - 1)
+            }, 1000)
+        )
     }, [startTime])
 
     useEffect(() => {
         if (countdown > 0) return
-        // stopGame()
-        clearInterval(interval)
+        stopGame()
+        clearInterval(intervalId)
     }, [countdown])
 
     return <div className="font-retro">{countdown}</div>
