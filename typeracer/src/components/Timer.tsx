d289cbe1b59a999d9734c526805409190874c767
Rendy Arya Kemal <renrror@gmail.com>
2022-03-05 11:54:36 +0700

    Fix timer not resetting at unlucky times

--- typeracer/src/components/Timer.tsx
+++ typeracer/src/components/Timer.tsx
@@ -2,18 +2,17 @@ import { FunctionalComponent } from 'preact'
 import { useEffect, useState } from 'preact/hooks'
 import { useGameContext } from '../context/GameContext'
 
+var intervalId: number
+
 const Timer: FunctionalComponent = () => {
     const [countdown, setCountdown] = useState(30)
-    const [intervalId, setIntervalId] = useState(0)
     const { startTime, stopGame } = useGameContext()
 
     useEffect(() => {
         if (startTime == 0) return
-        setIntervalId(
-            setInterval(() => {
-                setCountdown((current) => current - 1)
-            }, 1000)
-        )
+        intervalId = setInterval(() => {
+            setCountdown((current) => current - 1)
+        }, 1000)
     }, [startTime])
 
     useEffect(() => {
