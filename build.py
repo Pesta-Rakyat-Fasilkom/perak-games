b071f6de6703a1ebb2e07a207de2a8144af71b4e
emir.shamsuddin <emir.shamsuddin@ui.ac.id>
2023-03-25 19:45:44 +0700

    feat: snake nokia

--- build.py
+++ build.py
@@ -11,31 +11,32 @@ tetris_dir = Path("react-tetris").absolute()
 pacman_dir = Path("pacman-js").absolute()
 typeracer_dir = Path("typeracer").absolute()
 bad_apple_dir = Path("bad-canvas2d").absolute()
+snake_nokia_dir = Path("snake-nokia").absolute()
 result_dir = Path("result").absolute()
 
 # Flappy Bird
-flappy_dir = result_dir / "flappybird"
+# flappy_dir = result_dir / "flappybird"
 
-if result_dir.exists():
-    shutil.rmtree(result_dir)
+# if result_dir.exists():
+#     shutil.rmtree(result_dir)
 
-os.chdir(clumsy_dir)
+# os.chdir(clumsy_dir)
 
-res = subprocess.run("npx grunt-cli", shell=True)
-res.check_returncode()
+# res = subprocess.run("npx grunt-cli", shell=True)
+# res.check_returncode()
 
-os.chdir(cwd)
-result_dir.mkdir()
-flappy_dir.mkdir()
-(flappy_dir / "js").mkdir()
+# os.chdir(cwd)
+# result_dir.mkdir()
+# flappy_dir.mkdir()
+# (flappy_dir / "js").mkdir()
 
-copy_dirs = ["bower_components", "build", "data"]
-for dir in copy_dirs:
-    shutil.copytree(clumsy_dir / dir, flappy_dir / dir)
+# copy_dirs = ["bower_components", "build", "data"]
+# for dir in copy_dirs:
+#     shutil.copytree(clumsy_dir / dir, flappy_dir / dir)
 
-copy_files = ["app.json", "humans.txt", "index.css", "index.html", "js/melonJS-min.js"]
-for f in copy_files:
-    shutil.copy(clumsy_dir / f, flappy_dir / f)
+# copy_files = ["app.json", "humans.txt", "index.css", "index.html", "js/melonJS-min.js"]
+# for f in copy_files:
+#     shutil.copy(clumsy_dir / f, flappy_dir / f)
 
 
 # TETRIS
@@ -50,14 +51,14 @@ shutil.copytree(tetris_dir / "dist", result_dir / "tetris")
 shutil.copy(tetris_dir / "src" / "index.html", result_dir / "tetris" / "index.html")
 
 # PACMAN
-os.chdir(pacman_dir)
-res = subprocess.run("npx gulp", shell=True)
-res.check_returncode()
+# os.chdir(pacman_dir)
+# res = subprocess.run("npx gulp", shell=True)
+# res.check_returncode()
 
-copy_dirs = ["bower_components", "build", "app"]
-for dir in copy_dirs:
-    shutil.copytree(pacman_dir / dir, result_dir / "pacman" / dir)
-shutil.copy(pacman_dir / "index.html", result_dir / "pacman" / "index.html")
+# copy_dirs = ["bower_components", "build", "app"]
+# for dir in copy_dirs:
+#     shutil.copytree(pacman_dir / dir, result_dir / "pacman" / dir)
+# shutil.copy(pacman_dir / "index.html", result_dir / "pacman" / "index.html")
 
 # Typeracer
 os.chdir(typeracer_dir)
@@ -67,17 +68,27 @@ res.check_returncode()
 shutil.copytree(typeracer_dir / "dist", result_dir / "typeracer")
 
 # Bad Apple!!
-(result_dir / "bad-apple").mkdir()
+# (result_dir / "bad-apple").mkdir()
+# copied_files = [
+#     "index.html",
+#     "index.js",
+#     "style.css",
+#     "audio.ogg",
+#     "frames.gz",
+#     "lyrics.json",
+# ]
+# for fname in copied_files:
+#     shutil.copyfile(bad_apple_dir / fname, result_dir / "bad-apple" / fname)
+
+# Snake Nokia
+(result_dir / "snake-nokia").mkdir()
 copied_files = [
     "index.html",
-    "index.js",
-    "style.css",
-    "audio.ogg",
-    "frames.gz",
-    "lyrics.json",
+    "main.js",
+    "styles.css",
 ]
 for fname in copied_files:
-    shutil.copyfile(bad_apple_dir / fname, result_dir / "bad-apple" / fname)
+    shutil.copyfile(snake_nokia_dir / fname, result_dir / "snake-nokia" / fname)
 
 os.chdir(cwd)
 ghp_import(
